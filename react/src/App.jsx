13498ffbc12ae113b33a8451d30d8814d9247533
MoshiMoshiSan <Moshisan33@gmail.com>
2024-03-04 21:36:29 +0800

    Finished(?)

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,13 +1,70 @@
-im
+import axios from 'axios';
+import React from 'react';
 
-function App() {
-  const [count, setCount] = useState(0)
+class App extends React.Component {
 
-  return (
-    <>
-      
-    </>
-  )
+  state = { details: [], }
+
+  componentDidMount() {
+    axios.get('http://localhost:8000')
+      .then(res => {
+        this.setState({
+          details: res.data
+        });
+      })
+      .catch(err => { })
+  }
+
+  render() {
+    return (
+      <div>
+        <header> Employee Database </header>
+        <hr></hr>
+        <table>
+          <thead>
+            <tr>
+              <th>Employee Number</th>
+              <th>First Name</th>
+              <th>Middle Name</th>
+              <th>Last Name</th>
+              <th>Address</th>
+              <th>Barangay</th>
+              <th>Province</th>
+              <th>Country</th>
+              <th>Zipcode</th>
+              <th>Job Designation</th>
+              <th>Job Status</th>
+              <th>Department</th>
+              <th>Department Status</th>
+              <th>Employee Type</th>
+              <th>Employee Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.details.map((output, id) => (
+              <tr key={id}>
+                <td>{output.employee_number.employee_num}</td>
+                <td>{output.employee_number.first_name}</td>
+                <td>{output.employee_number.middle_name}</td>
+                <td>{output.employee_number.last_name}</td>
+                <td>{output.employee_number.address_line}</td>
+                <td>{output.employee_number.barangay}</td>
+                <td>{output.employee_number.province}</td>
+                <td>{output.employee_number.country}</td>
+                <td>{output.employee_number.zipcode}</td>
+                <td>{output.designation_id.job_name}</td>
+                <td>{output.designation_id.active_status ? 'Active' : 'Inactive'}</td>
+                <td>{output.designation_id.department_id.department_name}</td>
+                <td>{output.designation_id.department_id.active_status ? 'Active' : 'Inactive'}</td>
+                <td>{output.employee_type}</td>
+                <td>{output.status}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    )
+  }
 }
 
-export default App
+export default App;
